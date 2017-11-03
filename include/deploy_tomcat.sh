8c347a0022b6123bc1aaf7321bc0136fe3420d44
SuperNaturalGit <wangsongsong@wafersystems.com>
2017-11-03 10:57:36 +0800

    修改tomcat部署的方式，直接copy server下的所有文件

--- include/deploy_tomcat.sh
+++ include/deploy_tomcat.sh
@@ -8,17 +8,18 @@ Deploy_Tomcat(){
     [ -e "./config/tomcat/server.xml" ] && \cp -f ./config/tomcat/server.xml ${tomcat_install_dir}/conf/
 
     #部署项目
-    war_list=`find ./app -name "*.war"`
-    if [ ${#war_list[*]} -lt 1 ]; then
-      echo "Can't find any war package!"
-      exit 1
-    else
-      for war in ${war_list}
-      do
-        #${war}是查找到的war文件的相对路径。copy到tomcat的webapp目录下。
-        \cp -f "${war}" "${tomcat_install_dir}/webapps"
-      done
-    fi
+    \cp -f ./app/server/* "${tomcat_install_dir}/webapps"
+#    war_list=`find ./app -name "*.war"`
+#    if [ ${#war_list[*]} -lt 1 ]; then
+#      echo "Can't find any war package!"
+#      exit 1
+#    else
+#      for war in ${war_list}
+#      do
+#        #${war}是查找到的war文件的相对路径。copy到tomcat的webapp目录下。
+#        \cp -f "${war}" "${tomcat_install_dir}/webapps"
+#      done
+#    fi
     service tomcat start
   fi
 }
